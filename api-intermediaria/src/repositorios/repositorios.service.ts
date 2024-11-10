d996e2c2f69f0087805073c70fc834fe52d679c3
gabrielDellrado <grddourado@cosmobots.io>
2024-11-10 14:16:56 -0300

    fix: ajuste na busca de repositorios

--- api-intermediaria/src/repositorios/repositorios.service.ts
+++ api-intermediaria/src/repositorios/repositorios.service.ts
@@ -5,29 +5,33 @@ import axios from 'axios';
 export class RepositoriosService {
   async pegaRepositoriosCSharp() {
     try {
-      const url = 'https://api.github.com/users/takenet/repos?sort=created&direction=asc';
+      const url = 'https://api.github.com/orgs/takenet/repos?per_page=5&sort=created&direction=asc';
+      
       const headers = {
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
-      }
-      const response = await axios.get(url);
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        'User-Agent': 'NestJS-App',
+      };
+
+      const response = await axios.get(url, { headers });
       console.log("RESPONSE.DATA> ", response.data);
 
-      const repositoriosEmCSharp = response.data.filter(r => r.language === "C#");
+      const repositoriosEmCSharp = response.data.filter(r => r.language === 'C#');
 
       const responseParaChatBot = repositoriosEmCSharp.map(r => ({
         imagem: r.owner.avatar_url,
         titulo: r.full_name,
-        subtitulo: r.description,
-      }))
+        subtitulo: r.description || 'Sem descrição',
+      }));
 
-      return {status: 200, data: responseParaChatBot};
+      return { status: 200, data: responseParaChatBot };
 
     } catch (e) {
-      console.log("ERROR> ", e);
+      console.error("ERROR> ", e);
       return {
-        status: 500, 
-        message: "Erro ao buscar repositórios", 
-        message_alert: e.message};
+        status: 500,
+        message: 'Erro ao buscar repositórios',
+        message_alert: e.message,
+      };
     }
   }
 }
